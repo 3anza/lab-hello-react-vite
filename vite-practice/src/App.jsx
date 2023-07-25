cbdfe1808395a5c3e16edf6da0fc89fe3e495ec8
3anza <salmaaldubouni@gmail.com>
2023-07-25 09:52:23 +0200

    tried to make it look like the screenshot

---
+++ vite-practice/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import './index.css';
+
+
+function App() {
+  return (
+    <>
+    <nav>
+      <img src='https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png' alt='react is cool' />
+    <img src='https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-top-xs.png' alt='react is cool' />
+
+ </nav>
+    <div className="App"></div>
+    <h1>Say hello to ReactJS</h1>
+    <p>You will learn how to use the most popular frontend library, and become a super Ninja developer.</p>
+    <button>Awesome!</button>
+
+</>
+    
+  );
+}
+
+export default App;
